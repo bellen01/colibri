d69c378ae0b2f5e25c7fe971ba447643bf9ef8ed
Isabelle <isabelle.annie.karlsson@gmail.com>
2023-07-19 16:03:28 +0200

    changed styling for search bar

--- src/components/Header/SearchBar.tsx
+++ src/components/Header/SearchBar.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import styles from '@/components/styles/SearchBar.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const SearchBar = () => {
     return (
         <div>
             <div className={styles.wrapper}>
                 <input type="text" className={styles.input} />
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
             </div>
         </div>
     )
