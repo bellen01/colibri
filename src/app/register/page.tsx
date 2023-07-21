4647f5d1c071e97cbe644db321b67b5bdc6582ec
Isabelle <isabelle.annie.karlsson@gmail.com>
2023-07-21 14:10:52 +0200

    started with register component

---
+++ src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import RegisterComponent from '@/components/Register/RegisterComponent'
+import React from 'react'
+
+const register = () => {
+    return (
+        <div>
+            <RegisterComponent />
+        </div>
+    )
+}
+
+export default register
