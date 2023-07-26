56bf61a27ba344fdcf4e0d489f26591eb4a0b8db
Isabelle <isabelle.annie.karlsson@gmail.com>
2023-07-26 15:36:27 +0200

    created cart item component

--- src/components/Cart/CartItem.tsx
+++ src/components/Cart/CartItem.tsx
@@ -4,6 +4,9 @@ import { faTrashCan } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen } from '@fortawesome/free-solid-svg-icons';
 
+interface ICartItemProps {
+
+}
 
 const CartItem = () => {
     return (
