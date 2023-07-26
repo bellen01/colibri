af602f993d422f01de10ea0067a188304e7fc4a8
Isabelle <isabelle.annie.karlsson@gmail.com>
2023-07-26 15:35:36 +0200

    created favorites components

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styles from '@/components/styles/Favorites.module.scss';
+import FavoritesItem from './FavoritesItem';
 
 const Favorites = () => {
     return (
-        <div>Favorites</div>
+        <div>
+            <FavoritesItem />
+        </div>
     )
 }
 
