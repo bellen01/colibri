52524f6bf595bc46ce324ae41e8ca3145c3061f7
Isabelle <isabelle.annie.karlsson@gmail.com>
2023-08-30 09:58:38 +0200

    changed productfilter to use productcategories

---
+++ src/components/Products/ProductFilter.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+import styles from '@/components/styles/ProductFilter.module.scss';
+import Link from 'next/link';
+import { Poster, productCategories } from '@/types/Product.types';
+import { getPosters } from '@/app/posters/fetchFunctions';
+
+const ProductFilter = () => {
+
+    const [postersByCategory, setPostersByCategory] = useState<Poster[]>();
+
+    // const getPosterByCategory = async (categoryId: number) => {
+    //     try {
+    //         const posters = await getPosters();
+    //         const postersByCategory = posters?.filter((category) => category.category?.includes(categoryId))
+    //     console.log('postersbycategory', postersByCategory);
+    //     } catch (error) {
+    //         console.log('error in fetching posters by category', error)
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     getPosterByCategory()
+    // })
+
+    function capitalizeFirstLetter(word: string) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    }
+
+    return (
+        <aside className={styles.aside}>
+            <nav className={styles.nav}>
+                <Link href="/posters"><button className={styles.linkButton}>Alla</button></Link>
+                <Link href="/posters/topplistan"><button className={styles.linkButton}>Topplistan</button></Link>
+                <Link href="/posters/nyheter"><button className={styles.linkButton}>Nyheter</button></Link>
+                {productCategories.map((category) => (
+                    <Link href={`/posters/${category.categoryName}`} key={category.categoryId}><button className={styles.linkButton}>{capitalizeFirstLetter(category.categoryName)}</button></Link>
+
+                ))}
+                {/* <Link href="#">Svartvitt</Link>
+                <Link href="#">Landskap</Link>
+                <Link href="#">Blommor</Link>
+                <Link href="#">Droppar</Link>
+                <Link href="#">Insekter</Link>
+                <Link href="#">Djur</Link> */}
+            </nav>
+        </aside>
+    )
+}
+
+export default ProductFilter
