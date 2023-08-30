ca5a3daafac5317e5a20c0560627370e418e80af
Isabelle <isabelle.annie.karlsson@gmail.com>
2023-08-30 11:21:48 +0200

    added functionality and styling for showing active link in nav and productfilter

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -1,13 +1,21 @@
+"use client";
 import React from 'react'
 import Link from 'next/link'
 import styles from '@/components/styles/Nav.module.scss'
+import { useSelectedLayoutSegment, useSelectedLayoutSegments } from 'next/navigation';
 
 const Nav = () => {
+    // const activeSegment = useSelectedLayoutSegment();
+    const activeSegments = useSelectedLayoutSegments();
+    console.log('activesegments i nav', activeSegments);
+    // console.log('activesegment i nav', activeSegment)
+    const activeLink = activeSegments[0] == "posters" && activeSegments.length == 1;
+
     return (
         <nav className={styles.headerNav}>
-            <Link href="/posters">POSTERS</Link>
-            <Link href="/posters/topplistan">TOPPLISTAN</Link>
-            <Link href="#">NYHETER</Link>
+            <Link href="/posters" className={`${activeSegments[0] == "posters" && activeSegments.length == 1 ? styles.active : ""}`}>POSTERS</Link>
+            <Link href="/posters/topplistan" className={`${activeSegments[1] == "topplistan" ? styles.active : ""}`}>TOPPLISTAN</Link>
+            <Link href="/posters/nyheter" className={`${activeSegments[1] == "nyheter" ? styles.active : ""}`}>NYHETER</Link>
         </nav>
     )
 }
--- src/components/Products/ProductFilter.tsx
+++ src/components/Products/ProductFilter.tsx
@@ -4,8 +4,10 @@ import styles from '@/components/styles/ProductFilter.module.scss';
 import Link from 'next/link';
 import { Poster, productCategories } from '@/types/Product.types';
 import { getPosters } from '@/app/posters/fetchFunctions';
+import { useSelectedLayoutSegment } from 'next/navigation';
 
 const ProductFilter = () => {
+    const activeSegment = useSelectedLayoutSegment();
 
     const [postersByCategory, setPostersByCategory] = useState<Poster[]>();
 
@@ -27,14 +29,15 @@ const ProductFilter = () => {
         return word.charAt(0).toUpperCase() + word.slice(1);
     }
 
+
     return (
         <aside className={styles.aside}>
             <nav className={styles.nav}>
-                <Link href="/posters"><button className={styles.linkButton}>Alla</button></Link>
-                <Link href="/posters/topplistan"><button className={styles.linkButton}>Topplistan</button></Link>
-                <Link href="/posters/nyheter"><button className={styles.linkButton}>Nyheter</button></Link>
+                <Link href="/posters"><button className={`${styles.linkButton} ${activeSegment == null ? styles.active : ""}`}>Alla</button></Link>
+                <Link href="/posters/topplistan"><button className={`${styles.linkButton} ${activeSegment == "topplistan" ? styles.active : ""}`}>Topplistan</button></Link>
+                <Link href="/posters/nyheter"><button className={`${styles.linkButton} ${activeSegment == "nyheter" ? styles.active : ""}`}>Nyheter</button></Link>
                 {productCategories.map((category) => (
-                    <Link href={`/posters/${category.categoryName}`} key={category.categoryId}><button className={styles.linkButton}>{capitalizeFirstLetter(category.categoryName)}</button></Link>
+                    <Link href={`/posters/${category.categoryName}`} key={category.categoryId}><button className={`${styles.linkButton} ${activeSegment == category.categoryName ? styles.active : ""}`}>{capitalizeFirstLetter(category.categoryName)}</button></Link>
 
                 ))}
                 {/* <Link href="#">Svartvitt</Link>
